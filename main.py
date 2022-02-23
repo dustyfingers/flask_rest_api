b8e88ab6f4ee0e311242cab7ef2a8e85b1c96657
lou <dustyfingers01257@gmail.com>
2022-02-23 07:51:01 -0700

    learned about passing url params in flask

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ api = Api(app)
 class HelloWorld(Resource):
 
     # handles get method
-    def get(self):
-        return {"data": "GET to /helloworld"}
+    def get(self, user_name, test):
+        return {"user_name": user_name, "test": test}
 
     def post(self):
         return {"data": "POST to /helloworld!"}
 
 # register HelloWorld resource with the api
-api.add_resource(HelloWorld, "/helloworld")
+# <type: name> is the syntax for url params
+api.add_resource(HelloWorld, "/helloworld/<string:user_name>/<int:test>")
 
 # start app in debug mode (NOT for production environment)
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE_URL = "http://127.0.0.1:5000"
 
-response = requests.get(BASE_URL + "/helloworld")
+response = requests.get(BASE_URL + "/helloworld/lou/25")
 print(response.json())
