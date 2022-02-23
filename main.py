39ee5e8a4f73b355d1b19fc2e4f01430b4759385
lou <dustyfingers01257@gmail.com>
2022-02-22 21:36:23 -0700

    wrote simple hello world route and test!

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+# HelloWorld inherits from Resource class
+class HelloWorld(Resource):
+
+    # handles get method
+    def get(self):
+        return {"data": "Hello World"}
+
+# register HelloWorld resource with the api
+api.add_resource(HelloWorld, "/helloworld")
+
 # start app in debug mode (NOT for production environment)
 if __name__ == '__main__':
     app.run(debug=True)
