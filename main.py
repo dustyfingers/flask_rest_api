8d08f4fbb100b30304200ef1ea0789d64538e9e0
lou <dustyfingers01257@gmail.com>
2022-02-23 20:03:53 -0700

    built dummy json response

--- main.py
+++ main.py
@@ -5,19 +5,30 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {
+    "tim": {
+        "age": 19,
+        "gender": "male"
+    },
+    "lou": {
+        "age": 25,
+        "gender": "male"
+    }
+}
+
 # HelloWorld inherits from Resource class
 class HelloWorld(Resource):
 
     # handles get method
-    def get(self, user_name, test):
-        return {"user_name": user_name, "test": test}
+    def get(self, user_name):
+        return names[user_name]
 
     def post(self):
         return {"data": "POST to /helloworld!"}
 
 # register HelloWorld resource with the api
-# <type: name> is the syntax for url params
-api.add_resource(HelloWorld, "/helloworld/<string:user_name>/<int:test>")
+# <type: var_name> is the syntax for url params
+api.add_resource(HelloWorld, "/helloworld/<string:user_name>")
 
 # start app in debug mode (NOT for production environment)
 if __name__ == '__main__':
