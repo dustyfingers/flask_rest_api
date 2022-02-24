478531fc841df170746f292a258076f25e468326
lou <dustyfingers01257@gmail.com>
2022-02-23 20:14:45 -0700

    built first real endpoint

--- main.py
+++ main.py
@@ -1,34 +1,58 @@
-from flask import Flask 
+from flask import Flask, request
 from flask_restful import Api, Resource
 
 # create app & wrap it in an api
 app = Flask(__name__)
 api = Api(app)
 
-names = {
-    "tim": {
-        "age": 19,
-        "gender": "male"
-    },
-    "lou": {
-        "age": 25,
-        "gender": "male"
+# ! EXAMPLE
+
+# dummy json
+# names = {
+#     "tim": {
+#         "age": 19,
+#         "gender": "male"
+#     },
+#     "lou": {
+#         "age": 25,
+#         "gender": "male"
+#     }
+# }
+
+# # HelloWorld inherits from Resource class
+# class HelloWorld(Resource):
+
+#     # handles get method
+#     def get(self, user_name):
+#         return names[user_name]
+
+#     def post(self):
+#         return {"data": "POST to /helloworld!"}
+
+# # register HelloWorld resource with the api
+# # <type: var_name> is the syntax for url params
+# api.add_resource(HelloWorld, "/helloworld/<string:user_name>")
+
+# ! END EXAMPLE
+
+
+videos = {
+    "123": {
+        "likes": 50
     }
 }
 
-# HelloWorld inherits from Resource class
-class HelloWorld(Resource):
+class Video(Resource):
+    def get(self, video_id):
+        return videos[str(video_id)]
 
-    # handles get method
-    def get(self, user_name):
-        return names[user_name]
+    # FIXME: should actually be a post method 
+    def put(self, video_id):
+        print(request.form["likes"])
+        return videos[str(video_id)]
 
-    def post(self):
-        return {"data": "POST to /helloworld!"}
 
-# register HelloWorld resource with the api
-# <type: var_name> is the syntax for url params
-api.add_resource(HelloWorld, "/helloworld/<string:user_name>")
+api.add_resource(Video, "/videos/<int:video_id>")
 
 # start app in debug mode (NOT for production environment)
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import requests
 
 BASE_URL = "http://127.0.0.1:5000"
 
-response = requests.get(BASE_URL + "/helloworld/lou")
+response = requests.get(BASE_URL + "/videos/123")
+
+print(response.json())
+
+response = requests.put(BASE_URL + "/videos/456", { "likes": 100 })
 
 print(response.json())
